ad9a5ae11ebbe926b6643c38cb250234cc5e538f
sata lesmana <lesmanasata@gmail.com>
2026-06-15 19:00:52 +0700

    fix: register model

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,17 @@
 import type { Nitro } from 'nitropack';
 import mongoose from 'mongoose';
 
+// Register all Mongoose models before using populate()
+import '~/server/models/Activity.model';
+import '~/server/models/Customer.model';
+import '~/server/models/KategoriProduk.model';
+import '~/server/models/Produk.model';
+import '~/server/models/Satuan.model';
+import '~/server/models/StockBarang.model';
+import '~/server/models/User.model';
+import '~/server/models/UserActivity.model';
+import '~/server/models/Vendor.model';
+
 export default async (_nitroApp: Nitro) => {
   const config = useRuntimeConfig();
 
